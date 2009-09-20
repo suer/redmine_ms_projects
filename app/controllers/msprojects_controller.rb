8e84f1315e4f867e919829afe8d8c5cf87175974
sue <sue@barter.local>
2009-09-20 12:31:37 +0900

    add issues, but subject only..

--- app/controllers/msprojects_controller.rb
+++ app/controllers/msprojects_controller.rb
@@ -1,16 +1,29 @@
 class MsprojectsController < ApplicationController
-	before_filter :find_project, :authorize, :only => :index
+  before_filter :find_project, :authorize, :only => :index
 
   helper :msprojects
   include MsprojectsHelper
-	
+  
   def index
-		@project_id = params[:project_id]
-		@xml = ""
-		if request.post?
-			xml = params[:file][:msproject].read
-			parse_ms_project xml
-		end
+    @project_id = params[:project_id]
+    @project = Project.find @project_id
+    @xml = ""
+    tracker = Tracker.find 1
+    if request.post?
+      @xml = params[:file][:msproject].read
+      tasks = parse_ms_project(@xml)
+      tasks.each do |t|
+        param = {
+          :subject => t.name
+        }
+        issue = Issue.new param
+        issue.project = @project
+        issue.tracker = tracker
+        issue.author = User.current
+        issue.save!
+      end
+    end
+    
   end
 
   private
--- app/helpers/msprojects_helper.rb
+++ app/helpers/msprojects_helper.rb
@@ -1,7 +1,16 @@
+require "rexml/document" 
 module MsprojectsHelper
 
-	def parse_ms_project xml
-		
-	end
+  def parse_ms_project xml
+    tasks = []
+    doc = REXML::Document.new xml 
+    doc.elements.each('//Task') do |task_tag|
+      task = Task.new
+      name = task_tag.elements['Name']
+      task.name = name.text  if name
+      tasks << task
+    end
+    tasks
+  end
 
 end
