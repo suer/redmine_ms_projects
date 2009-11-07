b945e69eebf80a98f1f16205f5b66c95421e68a8
suer <sue@nagoya-u.jp>
2009-11-08 00:36:06 +0900

    modify description and increment version number

--- init.rb
+++ init.rb
@@ -3,8 +3,8 @@ require 'redmine'
 Redmine::Plugin.register :redmine_ms_projects do
   name 'Redmine Ms Projects plugin'
   author 'suer'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  description 'MS Project Importer for Redmine'
+  version '0.0.2'
 
   permission :msprojects, {:msprojects => [:index]}, :public => true
   menu :project_menu, :msprojects, { :controller => 'msprojects', :action => 'index' }, :caption => :msproject, :last => true, :param => :project_id
