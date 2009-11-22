5fca86b27b0b0aff4594eed542b0a1da9e64ad39
suer <kamakiri.t@gmail.com>
2009-11-22 17:40:53 +0900

    modify module setting

--- init.rb
+++ init.rb
@@ -6,7 +6,9 @@ Redmine::Plugin.register :redmine_ms_projects do
   description 'MS Project Importer for Redmine'
   version '0.0.2'
 
-  permission :msprojects, {:msprojects => [:index]}, :public => true
-  menu :project_menu, :msprojects, { :controller => 'msprojects', :action => 'index' }, :caption => :msproject, :last => true, :param => :project_id
+  project_module :msproject do
+    permission :msprojects, {:msprojects => [:index]}, :public => true
+  end
+  menu :project_menu, :msprojects, { :controller => 'msprojects', :action => 'index' }, :caption => :msproject, :param => :project_id
 
 end
